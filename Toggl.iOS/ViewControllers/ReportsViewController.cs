865eeb56990471e72d791b14fbe4cfc822f7d1cb
Desislav Hristov <hristov.desislav@gmail.com>
2019-11-20 20:34:23 +0300

    ✨ Create the VC itself

--- Toggl.iOS/ViewControllers/ReportsViewController.cs
+++ Toggl.iOS/ViewControllers/ReportsViewController.cs
@@ -1,25 +1,96 @@
 ﻿using System;
-
+using System.Reactive.Linq;
+using CoreGraphics;
+using Toggl.Core.UI.Extensions;
+using Toggl.Core.UI.ViewModels.Reports;
+using Toggl.iOS.Extensions;
+using Toggl.iOS.Extensions.Reactive;
+using Toggl.iOS.Presentation;
+using Toggl.iOS.Views.Reports;
+using Toggl.iOS.ViewSources;
+using Toggl.Shared.Extensions;
 using UIKit;
 
 namespace Toggl.iOS.ViewControllers
 {
-    public partial class ReportsViewController : UIViewController
+    public partial class ReportsViewController : ReactiveViewController<ReportsViewModel>, IScrollableToTop
     {
-        public ReportsViewController() : base("ReportsViewController", null)
+        private UIButton titleButton;
+
+        private ReportsCollectionViewRegularLayout regularLayout;
+        private ReportsCollectionViewCompactLayout compactLayout;
+
+        public ReportsViewController(ReportsViewModel viewModel) : base(viewModel, nameof(ReportsViewController))
         {
+        }
+
+        public override void TraitCollectionDidChange(UITraitCollection previousTraitCollection)
+        {
+            if (TraitCollection.HorizontalSizeClass == UIUserInterfaceSizeClass.Regular)
+                CollectionView.SetCollectionViewLayout(regularLayout, false);
+            else
+                CollectionView.SetCollectionViewLayout(compactLayout, false);
+            CollectionView.CollectionViewLayout.InvalidateLayout();
+            base.TraitCollectionDidChange(previousTraitCollection);
         }
 
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
+            prepareViews();
+
+            ViewModel.CurrentWorkspaceName
+                .Subscribe(WorkspaceLabel.Rx().Text())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.FormattedTimeRange
+                .Subscribe(titleButton.Rx().TitleAdaptive())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.HasMultipleWorkspaces
+                .Subscribe(WorkspaceButton.Rx().IsVisible())
+                .DisposedBy(DisposeBag);
+
+            WorkspaceButton.Rx()
+                .BindAction(ViewModel.SelectWorkspace)
+                .DisposedBy(DisposeBag);
+
+            regularLayout = new ReportsCollectionViewRegularLayout();
+            compactLayout = new ReportsCollectionViewCompactLayout();
+            var source = new ReportsCollectionViewSource(CollectionView);
+            if (TraitCollection.HorizontalSizeClass == UIUserInterfaceSizeClass.Regular)
+                CollectionView.SetCollectionViewLayout(regularLayout, false);
+            else
+                CollectionView.SetCollectionViewLayout(compactLayout, false);
+            CollectionView.Source = source;
+
+            ViewModel.Elements
+                .Subscribe(source.SetNewElements)
+                .DisposedBy(DisposeBag);
         }
 
-        public override void DidReceiveMemoryWarning()
+        public void ScrollToTop() { }
+
+        private void prepareViews()
         {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
+            var separator = NavigationController.NavigationBar.InsertSeparator();
+            separator.BackgroundColor = ColorAssets.OpaqueSeparator;
+
+            // Date range button
+            NavigationItem.TitleView = titleButton = new UIButton(new CGRect(0, 0, 200, 40));
+            titleButton.Font = UIFont.SystemFontOfSize(14, UIFontWeight.Medium);
+            titleButton.SetTitleColor(ColorAssets.Text, UIControlState.Normal);
+
+            // Workspace button settings
+            WorkspaceFadeView.FadeWidth = 32;
+            WorkspaceButton.Layer.ShadowColor = UIColor.Black.CGColor;
+            WorkspaceButton.Layer.ShadowRadius = 10;
+            WorkspaceButton.Layer.ShadowOffset = new CGSize(0, 2);
+            WorkspaceButton.Layer.ShadowOpacity = 0.10f;
+            WorkspaceButton.Layer.BorderColor = ColorAssets.Separator.CGColor;
+            WorkspaceButton.Layer.BorderWidth = 0.35f;
+
+            View.BackgroundColor = ColorAssets.TableBackground;
         }
     }
 }
--- Toggl.iOS/ViewControllers/ReportsViewController.designer.cs
+++ Toggl.iOS/ViewControllers/ReportsViewController.designer.cs
@@ -1,21 +1,50 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace Toggl.iOS.ViewControllers
 {
-    [Register("ReportsViewController")]
-    partial class ReportsViewController
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("ReportsViewController")]
+	partial class ReportsViewController
+	{
+		[Outlet]
+		UIKit.UICollectionView CollectionView { get; set; }
+
+		[Outlet]
+		UIKit.UIView WorkspaceButton { get; set; }
+
+		[Outlet]
+		Toggl.iOS.Views.FadeView WorkspaceFadeView { get; set; }
+
+		[Outlet]
+		UIKit.UILabel WorkspaceLabel { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (WorkspaceButton != null) {
+				WorkspaceButton.Dispose ();
+				WorkspaceButton = null;
+			}
+
+			if (WorkspaceFadeView != null) {
+				WorkspaceFadeView.Dispose ();
+				WorkspaceFadeView = null;
+			}
+
+			if (WorkspaceLabel != null) {
+				WorkspaceLabel.Dispose ();
+				WorkspaceLabel = null;
+			}
+
+			if (CollectionView != null) {
+				CollectionView.Dispose ();
+				CollectionView = null;
+			}
+		}
+	}
 }
