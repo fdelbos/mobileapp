bae4b2a1c6135cdb7883fa054989cf28558efb48
Desislav Hristov <hristov.desislav@gmail.com>
2019-11-20 20:33:37 +0300

    ✨ Create the new CV data source

---
+++ Toggl.iOS/ViewSources/ReportsCollectionViewSource.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using Foundation;
+using Toggl.Core.UI.ViewModels.Reports;
+using Toggl.iOS.Cells.Reports;
+using UIKit;
+
+namespace Toggl.iOS.ViewSources
+{
+    public class ReportsCollectionViewSource : UICollectionViewSource
+    {
+        private readonly UICollectionView collectionView;
+
+        private ImmutableList<IReportElement> elements;
+
+        private const string summaryCellIdentifier = "summaryCellIdentifier";
+        private const string barChartCellIdentifier = "barChartCellIdentifier";
+        private const string donutChartCellIdentifier = "donutChartCellIdentifier";
+        private const string projectCellIdentifier = "projectCellIdentifier";
+
+        public ReportsCollectionViewSource(UICollectionView collectionView)
+        {
+            this.collectionView = collectionView;
+
+            collectionView.RegisterNibForCell(ReportsSummaryCollectionViewCell.Nib, summaryCellIdentifier);
+            collectionView.RegisterNibForCell(ReportsBarChartCollectionViewCell.Nib, barChartCellIdentifier);
+            collectionView.RegisterNibForCell(ReportsDonutChartCollectionViewCell.Nib, donutChartCellIdentifier);
+            collectionView.RegisterNibForCell(ReportsProjectCollectionViewCell.Nib, projectCellIdentifier);
+        }
+
+        public void SetNewElements(IEnumerable<IReportElement> elements)
+        {
+            this.elements = elements.ToImmutableList();
+            collectionView.ReloadData();
+        }
+
+        public override UICollectionViewCell GetCell(UICollectionView collectionView, NSIndexPath indexPath)
+        {
+            UICollectionViewCell cell;
+            switch (indexPath.Item)
+            {
+                case 0:
+                    cell = collectionView.DequeueReusableCell(summaryCellIdentifier, indexPath) as UICollectionViewCell;
+                    // TODO: populate cell
+                    cell.BackgroundColor = UIColor.SystemBlueColor;
+                    return cell;
+                case 1:
+                    cell = collectionView.DequeueReusableCell(barChartCellIdentifier, indexPath) as UICollectionViewCell;
+                    // TODO: populate cell
+                    cell.BackgroundColor = UIColor.SystemGreenColor;
+                    return cell;
+                case 2:
+                    cell = collectionView.DequeueReusableCell(donutChartCellIdentifier, indexPath) as UICollectionViewCell;
+                    // TODO: populate cell
+                    cell.BackgroundColor = UIColor.SystemRedColor;
+                    return cell;
+                default:
+                    cell = collectionView.DequeueReusableCell(projectCellIdentifier, indexPath) as UICollectionViewCell;
+                    // TODO: populate cell
+                    cell.BackgroundColor = UIColor.SystemYellowColor;
+                    return cell;
+            }
+        }
+
+        public override nint NumberOfSections(UICollectionView collectionView)
+        {
+            return 1;
+        }
+
+        public override nint GetItemsCount(UICollectionView collectionView, nint section)
+        {
+            // TODO: replace with actual elements count
+            return 33;
+        }
+    }
+}
