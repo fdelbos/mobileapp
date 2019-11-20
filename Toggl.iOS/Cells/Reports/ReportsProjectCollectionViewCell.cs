0b6210a06c65068b21eef25bdfe3cae39bd00838
Desislav Hristov <hristov.desislav@gmail.com>
2019-11-20 20:32:25 +0300

    ✨ Create the new CV cells

---
+++ Toggl.iOS/Cells/Reports/ReportsProjectCollectionViewCell.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace Toggl.iOS.Cells.Reports
+{
+    public partial class ReportsProjectCollectionViewCell : UICollectionViewCell
+    {
+        public static readonly NSString Key = new NSString("ReportsProjectCollectionViewCell");
+        public static readonly UINib Nib;
+        public static readonly int Height = 56;
+
+        static ReportsProjectCollectionViewCell()
+        {
+            Nib = UINib.FromName("ReportsProjectCollectionViewCell", NSBundle.MainBundle);
+        }
+
+        protected ReportsProjectCollectionViewCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+    }
+}
+
